3d69ae235916073b857461fed6ea76a9692159a1
daisuke iba <d.iba@futurek.com>
2024-09-04 14:34:14 +0900

    add: sass

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @use "sass:math";
+          `
+        }
+      }
+    }
+  }
 })
